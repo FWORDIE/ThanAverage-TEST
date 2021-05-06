178a84bc69ee8669adf7788da4305027208d3175
Fred Wrodie <fred@mildlyupset.com>
2021-05-06 10:27:54 +0200

    New Fonts

--- Scripts/Quiz.js
+++ Scripts/Quiz.js
@@ -15,7 +15,7 @@ const ListBox = document.getElementById("ListBox");
 const AboutArea = document.getElementById("AboutArea");
 const NumofQues = questions.length;
 var QNum = 0;
-var UsedQues = [];
+var UsedQues = [27];
 //var UsedQues = [1, 2, 3, 4, 5, 6, 7, 8, 9,10,11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43];
 
 // Load New Question Number
