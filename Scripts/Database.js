b5110c31182e284fcfd872d6f937c6c7c00f8dfe
Jae Perris <jae.perris@gmail.com>
2021-05-20 18:12:26 +0200

    Update database

--- Scripts/Database.js
+++ Scripts/Database.js
@@ -1,29 +1,28 @@
-// Load Fauna Stuff
 let NoCalls = 0;
 let NoPosts = 0;
 
 // Load DataBase from Fauna /// change to read only only on question
 function GetDataBase() {
-NoCalls++;
-fetch("http://167.71.78.185/questions").then(async (response) => {
-questions = await response.json();
-console.log(`Got questions, ${questions.length}`);
-});
+  NoCalls++;
+  fetch("https://backend.thanaverage.xyz/questions").then(async (response) => {
+    questions = await response.json();
+    console.log(`Got questions, ${questions.length}`);
+  });
 }
 
 // Update dataBase
 /**
-* @param {number} questionNumber
-* @param {'yes'| 'no'} answer
-*/
+ * @param {number} questionNumber
+ * @param {'yes'| 'no'} answer
+ */
 function UpdateDataBase(questionNumber, answer) {
-NoPosts++;
-logger("GetDataBase", `Calls: ${NoCalls}, Posts: ${NoPosts}`);
-fetch("http://167.71.78.185/answer", {
-method: "POST",
-headers: {
-"Content-Type": "application/json",
-},
-body: JSON.stringify({ questionNumber: questionNumber, answer: answer }),
-}).then(async (res) => console.log(await res.text()));
+  NoPosts++;
+  logger("GetDataBase", `Calls: ${NoCalls}, Posts: ${NoPosts}`);
+  fetch("https://backend.thanaverage.xyz/answer", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ questionNumber: questionNumber, answer: answer }),
+  }).then(async (res) => console.log(await res.text()));
 }
