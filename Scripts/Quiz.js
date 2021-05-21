5f35824b0dd3cf85aa6cc30050bd38125a459c81
Fred Wrodie <fred@mildlyupset.com>
2021-05-21 19:44:19 +0200

    fixed 100

--- Scripts/Quiz.js
+++ Scripts/Quiz.js
@@ -15,22 +15,22 @@ const ListBox = document.getElementById("ListBox");
 const AboutArea = document.getElementById("AboutArea");
 const NumofQues = questions.length;
 var QNum = 0;
-var UsedQues = [];
+var UsedQues = [999];
 
-if(sessionStorage.getItem("localUsedQues" === null)){
+if(JSON.parse(sessionStorage.getItem("localUsedQues")) === null){
     UsedQues = [];
 }else{
-    UsedQues = [sessionStorage.getItem("localUsedQues")];
+    UsedQues = JSON.parse(sessionStorage.getItem("localUsedQues"));
 }
 //var UsedQues = [1, 2, 3, 4, 5, 6, 7, 8, 9,10,11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43];
 
 // Load New Question Number
 function NewQnum() {
-    QNum = Math.floor(Math.random() * (questions.length-1));
+    QNum = Math.floor(Math.random() * (questions.length));
     var check = UsedQues.includes(QNum);
     if (check === false) {
         UsedQues.push(QNum);
-        sessionStorage.setItem('localUsedQues', UsedQues);
+        sessionStorage.setItem('localUsedQues', JSON.stringify(UsedQues));
         logger('NewQnum(usedArr',UsedQues);
         logger('NewQnum',QNum);
         return QNum;
@@ -41,7 +41,7 @@ function NewQnum() {
             logger("Used NewQnum", QNum);
             if(check === false){
                 UsedQues.push(QNum);
-                sessionStorage.setItem('localUsedQues', UsedQues);
+                sessionStorage.setItem('localUsedQues', JSON.stringify(UsedQues));
                 logger('NewQnum(usedArr',UsedQues);
                 logger('NewQnum',QNum);
                 return QNum;
